33c4b09cb9c5648e55f9dabd6ab40928665778fb
♰ĐɆɃɌØN♰ <alexdebron2006@gmail.com>
2025-12-02 02:02:52 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "image2prompt-api",
+  "version": "1.0.0",
+  "description": "Image to Prompt API by Chamod Nimsara",
+  "main": "index.js",
+  "scripts": {
+    "dev": "vercel dev",
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.0"
+  },
+  "engines": {
+    "node": "18.x"
+  }
+}
